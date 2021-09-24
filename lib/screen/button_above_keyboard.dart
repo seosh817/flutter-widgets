c9c2fdf5ff4fd059b1e4c31dd17b1f7037978bac
seunghwan <slflfl12@naver.com>
2021-09-25 00:27:14 +0900

    button above keyboard

--- lib/screen/button_above_keyboard.dart
+++ lib/screen/button_above_keyboard.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_widgets/colors.dart';
@@ -8,7 +6,6 @@ import '../text_style.dart';
 
 class ButtonAboveKeyboardScreen extends StatefulWidget {
   static String routeName = '/button_above_keyboard_page';
-
 
   @override
   _ButtonAboveKeyboardPageState createState() => _ButtonAboveKeyboardPageState();
@@ -21,22 +18,18 @@ class _ButtonAboveKeyboardPageState extends State<ButtonAboveKeyboardScreen> {
   bool _isValid;
   bool _isNewObscure;
 
-  bool _isNewConfirmValid;
+  bool _isPasswordValid;
   bool _isPasswordObscure;
-
-
 
   @override
   void initState() {
     super.initState();
     _isValid = false;
     _isNewObscure = true;
-    _isNewConfirmValid = false;
+    _isPasswordValid = false;
     _isPasswordObscure = true;
-    _idController.addListener(() {
-    });
-    _passWordController.addListener(() {
-    });
+    _idController.addListener(() {});
+    _passWordController.addListener(() {});
   }
 
   @override
@@ -45,7 +38,6 @@ class _ButtonAboveKeyboardPageState extends State<ButtonAboveKeyboardScreen> {
     _passWordController.dispose();
     super.dispose();
   }
-
 
   void _passwordObscureToggle() {
     setState(() {
@@ -56,151 +48,158 @@ class _ButtonAboveKeyboardPageState extends State<ButtonAboveKeyboardScreen> {
   @override
   Widget build(BuildContext context) {
     final node = FocusScope.of(context);
-    var appBar = AppBar(
-      backgroundColor: Colors.white,
-      elevation: 0.0,
-      centerTitle: true,
-      title: Text(
-        'Login Screen',
-        style: kNotoSansMedium16.copyWith(color: AppColors.dark),
-      ),
-      leading: IconButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          icon: Image.asset('images/ic_back.png')),
-    );
     return GestureDetector(
       onTap: () {
         node.requestFocus(FocusNode());
       },
       child: Scaffold(
-        resizeToAvoidBottomInset: true,
+        resizeToAvoidBottomInset: false,
         backgroundColor: Colors.white,
-        appBar: appBar,
-        body: Container(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    RichText(
-                      text: TextSpan(children: [
-                        TextSpan(text: 'ButtonAboveKeyboard Example', style: kNotoSansBold24.copyWith(color: AppColors.dark)),
-                      ]),
-                    ),
-                    SizedBox(
-                      height: 36.0,
-                    ),
-                    TextFormField(
-                      autovalidateMode: AutovalidateMode.onUserInteraction,
-                      controller: _idController,
-                      keyboardType: TextInputType.name,
-                      obscureText: _isNewObscure,
-                      style: kNotoSansMedium16.copyWith(color: AppColors.dark),
-                      textInputAction: TextInputAction.next,
-                      onEditingComplete: _isValid
-                          ? () {
-                        node.nextFocus();
-                      }
-                          : null,
-                      autofocus: false,
-                      cursorColor: AppColors.azul,
-                      decoration: InputDecoration(
-                        counterText: ' ',
-                        labelText: "ID",
-                        labelStyle: kNotoSansMedium12.copyWith(
-                          color: AppColors.lightBlueGrey,
-                        ),
-                        hintText: 'ID',
-                        hintStyle: kNotoSansMedium16.copyWith(
-                          color: AppColors.lightBlueGrey,
-                        ),
-                        focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(
-                            color: AppColors.azul,
-                            width: 1.5,
-                          ),
-                        ),
-                        border: UnderlineInputBorder(
-                          borderSide: BorderSide(
-                            color: AppColors.lightBlueGrey,
-                            width: 1.0,
-                          ),
-                        ),
-                        alignLabelWithHint: true,
-                        isDense: true,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    TextFormField(
-                      autovalidateMode: AutovalidateMode.onUserInteraction,
-                      controller: _passWordController,
-                      keyboardType: TextInputType.visiblePassword,
-                      // validator: (String value) => passwordConfirmValidator(value, _newPasswordController.text),
-                      obscureText: _isPasswordObscure,
-                      style: kNotoSansMedium16.copyWith(color: AppColors.dark),
-                      textInputAction: TextInputAction.done,
-                      onEditingComplete: _isNewConfirmValid
-                          ? () {
-                        node.unfocus();
-                      }
-                          : null,
-                      autofocus: false,
-                      cursorColor: AppColors.azul,
-                      decoration: InputDecoration(
-                        labelText: "Password",
-                        labelStyle: kNotoSansMedium12.copyWith(
-                          color: AppColors.lightBlueGrey,
-                        ),
-                        hintText: 'Password',
-                        focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(
-                            color: AppColors.azul,
-                            width: 1.5,
-                          ),
-                        ),
-                        border: UnderlineInputBorder(
-                          borderSide: BorderSide(
-                            color: AppColors.lightBlueGrey,
-                            width: 1.0,
-                          ),
-                        ),
-                        alignLabelWithHint: true,
-                        isDense: true,
-                        suffixIcon: _isPasswordObscure
-                            ? IconButton(
-                          icon: Image.asset('images/invisible_icon.png'),
-                          onPressed: () => _passwordObscureToggle(),
-                        )
-                            : IconButton(
-                          icon: Image.asset('images/visible_icon.png'),
-                          onPressed: () => _passwordObscureToggle(),
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 400,),
-                    Container(
-                      width: double.infinity,
-                      color: Colors.white,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          padding: EdgeInsets.symmetric(vertical: 14),
-                          primary: AppColors.azul,
-                          onSurface: AppColors.blueGrey,
-                        ),
-                        child: Text('Login'),
-                      ),
-                    ),
-                  ],
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0.0,
+          centerTitle: true,
+          title: Text(
+            'Login Screen',
+            style: kNotoSansMedium16.copyWith(color: AppColors.dark),
+          ),
+          leading: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: Image.asset('images/ic_back.png')),
+        ),
+        bottomSheet: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+            child: Container(
+              width: double.infinity,
+              color: Colors.white,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(vertical: 14),
+                  primary: AppColors.azul,
+                  onSurface: AppColors.blueGrey,
                 ),
+                child: Text('Login'),
               ),
-            ],
+            ),
+          ),
+        ),
+        body: SingleChildScrollView(
+          reverse: false,
+          physics: BouncingScrollPhysics(),
+          child: Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      RichText(
+                        text: TextSpan(children: [
+                          TextSpan(text: 'ButtonAboveKeyboard Example', style: kNotoSansBold24.copyWith(color: AppColors.dark)),
+                        ]),
+                      ),
+                      SizedBox(
+                        height: 36.0,
+                      ),
+                      TextFormField(
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        controller: _idController,
+                        keyboardType: TextInputType.name,
+                        obscureText: _isNewObscure,
+                        style: kNotoSansMedium16.copyWith(color: AppColors.dark),
+                        textInputAction: TextInputAction.next,
+                        onEditingComplete: _isValid
+                            ? () {
+                                node.nextFocus();
+                              }
+                            : null,
+                        autofocus: false,
+                        cursorColor: AppColors.azul,
+                        decoration: InputDecoration(
+                          counterText: ' ',
+                          labelText: "ID",
+                          labelStyle: kNotoSansMedium12.copyWith(
+                            color: AppColors.lightBlueGrey,
+                          ),
+                          hintText: 'ID',
+                          hintStyle: kNotoSansMedium16.copyWith(
+                            color: AppColors.lightBlueGrey,
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: AppColors.azul,
+                              width: 1.5,
+                            ),
+                          ),
+                          border: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: AppColors.lightBlueGrey,
+                              width: 1.0,
+                            ),
+                          ),
+                          alignLabelWithHint: true,
+                          isDense: true,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      TextFormField(
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        controller: _passWordController,
+                        keyboardType: TextInputType.visiblePassword,
+                        // validator: (String value) => passwordConfirmValidator(value, _newPasswordController.text),
+                        obscureText: _isPasswordObscure,
+                        style: kNotoSansMedium16.copyWith(color: AppColors.dark),
+                        textInputAction: TextInputAction.done,
+                        onEditingComplete: _isPasswordValid
+                            ? () {
+                                node.unfocus();
+                              }
+                            : null,
+                        autofocus: false,
+                        cursorColor: AppColors.azul,
+                        decoration: InputDecoration(
+                          labelText: "Password",
+                          labelStyle: kNotoSansMedium12.copyWith(
+                            color: AppColors.lightBlueGrey,
+                          ),
+                          hintText: 'Password',
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: AppColors.azul,
+                              width: 1.5,
+                            ),
+                          ),
+                          border: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: AppColors.lightBlueGrey,
+                              width: 1.0,
+                            ),
+                          ),
+                          alignLabelWithHint: true,
+                          isDense: true,
+                          suffixIcon: _isPasswordObscure
+                              ? IconButton(
+                                  icon: Image.asset('images/invisible_icon.png'),
+                                  onPressed: () => _passwordObscureToggle(),
+                                )
+                              : IconButton(
+                                  icon: Image.asset('images/visible_icon.png'),
+                                  onPressed: () => _passwordObscureToggle(),
+                                ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
